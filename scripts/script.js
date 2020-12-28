ce848640d97c6c1a34e1d02d7439a8b29389c5dd
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2020-12-28 16:35:48 +0100

    add scrolling top arrow

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
 import menu from './menu.js'
+import scrollingTop from './scrollingTop.js'
 
 menu()
+scrollingTop()
---
+++ scripts/scrollingTop.js
@@ -0,0 +1,18 @@
+const scrollingTop = () =>{
+    const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    var target = document.querySelector('.arrow-top');
+    window.addEventListener("scroll",function(){
+    if(window.pageYOffset >= vh*0.5){
+    target.style.display = "flex";
+    }
+    else if(window.pageYOffset < vh*0.5){
+        target.style.display = "none";
+    }
+    },false); 
+
+
+    target.addEventListener("click", function(){
+    window.scrollTo({top: 0, left: 0});
+    });
+}
+export default scrollingTop;
