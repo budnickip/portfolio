1f2514c60ab188744e14a94e27273e2b7917d21d
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2020-12-28 15:05:49 +0100

    working menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,13 +1,16 @@
 const menu = () =>{
     const menuBtn = document.querySelector('.menu-btn')
+    const navbarLinks = document.querySelector('.navigation__items')
     let menuOpen = false
 
     menuBtn.addEventListener('click', ()=>{
         if(!menuOpen){
             menuBtn.classList.add('open')
+            navbarLinks.classList.add('active')
             menuOpen = true
         } else{
             menuBtn.classList.remove('open')
+            navbarLinks.classList.remove('active')
             menuOpen = false
         }
     })
