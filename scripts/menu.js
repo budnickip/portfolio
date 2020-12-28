c66788b3f9d59a0c4e46cf4b401245ac66883867
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2020-12-28 14:32:21 +0100

    menu button with animation

---
+++ scripts/menu.js
@@ -0,0 +1,17 @@
+const menu = () =>{
+    const menuBtn = document.querySelector('.menu-btn')
+    let menuOpen = false
+
+    menuBtn.addEventListener('click', ()=>{
+        if(!menuOpen){
+            menuBtn.classList.add('open')
+            menuOpen = true
+        } else{
+            menuBtn.classList.remove('open')
+            menuOpen = false
+        }
+    })
+}
+
+export default menu
+
