b8937477eec3cc041d4bf8ab7c92ff7b6bea8c3d
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2021-01-25 18:53:01 +0100

    add webpack

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import './styles/style.scss';
+
+import menu from './scripts/menu.js'
+import scrollingTop from './scripts/scrollingTop.js'
+import hideNavbar from './scripts/hideNavbar.js'
+import delayIcons from './scripts/delayIcons.js'
+import sendingMail from './scripts/sendingMail.js'
+import validateForm from './scripts/validateForm.js'
+import type from './scripts/typing.js'
+
+
+menu()
+setTimeout(type, 1300)
+scrollingTop()
+hideNavbar()
+delayIcons()
+sendingMail()
+validateForm()
+
+
+
+
