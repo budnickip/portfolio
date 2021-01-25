1fe7ac09d9ce8a3c0268cec120b3dd367e9e1843
LAPTOP-738OA63Q\Patryk <p-budnicki@wp.pl>
2021-01-25 12:06:22 +0100

    new hover in nav

--- scripts/script.js
+++ scripts/script.js
@@ -4,52 +4,17 @@ import hideNavbar from './hideNavbar.js'
 import delayIcons from './delayIcons.js'
 import sendingMail from './sendingMail.js'
 import validateForm from './validateForm.js'
+import type from './typing.js'
 
 
 menu()
+setTimeout(type, 1300)
 scrollingTop()
 hideNavbar()
 delayIcons()
 sendingMail()
 validateForm()
 
-const typedTextSpan = document.querySelector('.baner-text__typed-text')
-const cursorSpan = document.querySelector('.baner-text__cursor')
-const textArray = ["Patryk", "IT engineer", "Front-end Developer", "programming enthusiast"]
-const typingDelay = 200;
-const erasingDelay = 100;
-const newTextDelay = 2000;
-let textArrayIndex = 0;
-let charIndex = 0;
 
-function type() {
-    if(charIndex < textArray[textArrayIndex].length){
-        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing")
-        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex)
-        charIndex++;
-        setTimeout(type, typingDelay)
-    }else{
-        cursorSpan.classList.remove("typing")
-        setTimeout(erase, newTextDelay)
-    }
-}
-
-function erase() {
-    if(charIndex > 0){
-        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing")
-        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1)
-        charIndex--;
-        setTimeout(erase, erasingDelay)
-    }else{
-        cursorSpan.classList.remove("typing")
-        textArrayIndex++;
-        if(textArrayIndex>=textArray.length) textArrayIndex = 0
-        setTimeout(type, typingDelay + 1100)
-    }
-}
-
-document.addEventListener("DOMContentLoaded", function(){
-    setTimeout(type, newTextDelay + 250)
-}) 
 
 
--- scripts/typing.js
+++ scripts/typing.js
@@ -1,5 +1,5 @@
 const typedTextSpan = document.querySelector('.baner-text__typed-text')
-
+const cursorSpan = document.querySelector('.baner-text__cursor')
 const textArray = ["Patryk", "IT engineer", "Front-end Developer", "programming enthusiast"]
 const typingDelay = 200;
 const erasingDelay = 100;
@@ -9,28 +9,28 @@ let charIndex = 0;
 
 function type() {
     if(charIndex < textArray[textArrayIndex].length){
+        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing")
         typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex)
         charIndex++;
         setTimeout(type, typingDelay)
     }else{
+        cursorSpan.classList.remove("typing")
         setTimeout(erase, newTextDelay)
     }
 }
 
 function erase() {
     if(charIndex > 0){
+        if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing")
         typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1)
         charIndex--;
         setTimeout(erase, erasingDelay)
     }else{
+        cursorSpan.classList.remove("typing")
         textArrayIndex++;
         if(textArrayIndex>=textArray.length) textArrayIndex = 0
-        setTimeout(type, typingDelay)
+        setTimeout(type, typingDelay + 1100)
     }
 }
-
-document.addEventListener("DOMContentLoaded", function(){
-    setTimeout(type, newTextDelay + 250)
-}) 
 
 export default type
